cd6ce454d9dce8bd856e1c92e9256c0c4b392341
Zyphirex <axiom1@yahoo.com>
2013-09-01 18:38:10 -0400

    Finish mjoin

--- config/mafia.js
+++ config/mafia.js
@@ -15,6 +15,7 @@ var mafia = exports.mafia = {
 	mafiaThemes : JSON.parse(fs.readFileSync('config/mafiathemes.json')),
 	theme : {},
 	timer : setTimeout(function(){},0),
+	maxPlayers : 0,
 	// Modify this pathway to change the mafia room.
 	room : require('../rooms.js').rooms.mafia,
 	
@@ -41,6 +42,13 @@ var mafia = exports.mafia = {
 	
 	gameSignUp : function(user, theme) {
 		mafia.theme = mafia.mafiaThemes[theme];
+		// Determine the maximum number of players for the theme.
+		var rolesListNum = 1;
+		while (('roles' + rolesListNum) in mafia.theme) {
+			rolesListNum++;
+		}
+		mafia.maxPlayers = mafia.theme['roles' + (rolesListNum-1)];
+		// Announce the game.
 		room.add(user + ' has started a new mafia game with theme ' + mafa.theme.name);
 		room.add('Type //mjoin to join the game.');
 		// If the theme has a summary, it will be posted.
@@ -67,10 +75,12 @@ var commands = exports.commands = {
 			return this.sendReply('You cannot start a mafia game because you are muted.');
 		}
 		if (mafia.gameRunning) {
-			return this.sendReply('A game is currently being played. Use /mjoin to join the next game.');
+			this.sendReply('A game is currently being played.');
+			return this.sendReply('Wait until the game is over to start a new one.');
 		}
 		if (mafia.gameStarting) {
-			return this.sendReply('A game is currently in its sign-up phase. Use /mjoin to join.');
+			this.sendReply('A game is currently in its sign-up phase.');
+			return this.sendReply('Type /mjoin to join.');
 		}
 		// Theme defaults to default (surprise).
 		if (!target) {
@@ -86,6 +96,26 @@ var commands = exports.commands = {
 	
 	mjoin: function(target, room, user, connection) {
 		// Make sure the user isn't doing something they aren't supposed to be doing.
-		
+		if (room !== mafia.room) { return; }
+		if (!this.canTalk()) {
+			return this.sendReply('You cannot join the mafia game because you are muted.');
+		}
+		if (mafia.gameRunning) {
+			this.sendReply('A game is currently being played.');
+			return this.sendReply('Use /mjoin to join the next game.');
+		}
+		if (!mafia.gameStarting) {
+			this.sendReply('There is no mafia game in its Sign-Up phase right now.');
+			return this.sendReply('Type /mstart to start a new game.');
+		}
+		if (mafia.players.indexOf(user) !== -1) {
+			return this.sendReply('You have already joined the current game.');
+		}
+		mafia.players.push(user);
+		this.sendReply('You have joined the mafia game.');
+		if (mafia.players.length >== mafia.maxPlayers) {
+			return room.add('Sign-Ups have closed.');
+		}
+		return;
 	}
 };
